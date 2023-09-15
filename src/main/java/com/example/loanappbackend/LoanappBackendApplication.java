2fd4b2146fa57cb6c5f16492b8c2144e71ea08fb
tyagianurag477 <anuragtyagi477@gmail.com>
2023-09-16 02:33:04 +0530

    Unit Testing for EmployeeService Successful

--- src/main/java/com/example/loanappbackend/LoanappBackendApplication.java
+++ src/main/java/com/example/loanappbackend/LoanappBackendApplication.java
@@ -2,12 +2,15 @@ package com.example.loanappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class LoanappBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanappBackendApplication.class, args);
+		System.out.println("Hello Spring Boot");
 	}
 
 }
---
+++ src/test/java/com/example/loanappbackend/service/EmployeeServiceTest.java
@@ -0,0 +1,85 @@
+package com.example.loanappbackend.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.loanappbackend.model.Employee;
+import com.example.loanappbackend.repository.EmployeeRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+	
+	@Mock
+	private EmployeeRepository employeeRepository;
+	
+	@InjectMocks
+	private EmployeeServiceImpl employeeService;
+	
+	private Employee employee;
+	
+	@BeforeEach
+	void setUp() {
+		this.employee=new Employee("E1","anurag","manager","dti",'M',LocalDate.now(),LocalDate.now());
+	}
+	
+	
+	//JUnit test for save employee method
+	@Test
+	void saveEmployee() {
+		
+		
+		//
+		when(employeeRepository.save(employee)).thenReturn(employee);
+		System.out.println(employeeRepository);
+		System.out.println(employeeService);
+		
+		Employee savedEmployee=employeeService.saveEmployee(employee);
+		
+		System.out.println(savedEmployee);
+		assertThat(savedEmployee).isNotNull();
+	}
+	
+	@Test
+	void findEmployeeById() {
+		
+		when(employeeRepository.findById("E1")).thenReturn(Optional.of(employee));
+		
+		System.out.println(employeeRepository);
+		System.out.println(employeeService);
+		
+		Employee fetchedEmployee=employeeService.getEmployeeById("E1");
+		System.out.println(fetchedEmployee);
+		assertThat(fetchedEmployee).isNotNull();
+	}
+
+//	@Test
+//	void deleteEmployeeById() {
+//		
+//		
+//		//Mockito‘s doNothing() is used when you want to test void methods because 
+//		//void methods do not return anything so there is no way you can verify using assert.
+//		doNothing().when(employeeRepository).deleteById("E1");
+//		
+//		employeeService.deleteEmployeeById("E1");
+//		
+//		/*Mockito verify() method can be used to test number of method invocations too. 
+//		 * We can test exact number of times, at least once, at least, at most number of
+//		 *  invocation times for a mocked method.*/
+//		
+//		verify(employeeRepository,times(1)).deleteById("E1");
+//	}
+	
+}
